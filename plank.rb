627cee0976f491e93ee6a757b028577c262dd209
Rahul Malik <rmalik@pinterest.com>
2017-12-15 09:48:28 -0800

    Update plank Formula to v1.2

--- plank.rb
+++ plank.rb
@@ -1,5 +1,5 @@
 class Plank < Formula
-  plank_version = "v1.1"
+  plank_version = "v1.2"
   desc "Framework for generating immutable model objects"
   homepage "https://pinterest.github.io/plank/"
   url "https://github.com/pinterest/plank.git", :tag => plank_version
