68e7cbc24cf35567d060083cdab931f2d0ebc3e3
Gabriel Lievano <glievano@pinterest.com>
2022-08-05 11:11:17 -0700

    Add xcodegen formula

---
+++ Formula/xcodegen.rb
@@ -0,0 +1,39 @@
+class Xcodegen < Formula
+  desc "Generate your Xcode project from a spec file and your folder structure"
+  homepage "https://github.com/yonaskolb/XcodeGen"
+  url "https://github.com/pinterest/XcodeGen/archive/2.31.1.tar.gz"
+  sha256 "b1a55ac98e402b4caafac6b320ccdbbc7dcea40020c643a12712b32785865a89"
+  license "MIT"
+  head "https://github.com/pinterest/XcodeGen.git", branch: "master"
+
+  depends_on xcode: ["10.2", :build]
+  depends_on macos: :catalina
+
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/#{name}"
+    pkgshare.install "SettingPresets"
+  end
+
+  test do
+    (testpath/"xcodegen.yml").write <<~EOS
+      name: GeneratedProject
+      options:
+        bundleIdPrefix: com.project
+      targets:
+        TestProject:
+          type: application
+          platform: iOS
+          sources: TestProject
+    EOS
+    (testpath/"TestProject").mkpath
+    system bin/"xcodegen", "--spec", testpath/"xcodegen.yml"
+    assert_predicate testpath/"GeneratedProject.xcodeproj", :exist?
+    assert_predicate testpath/"GeneratedProject.xcodeproj/project.pbxproj", :exist?
+    output = (testpath/"GeneratedProject.xcodeproj/project.pbxproj").read
+    assert_match "name = TestProject", output
+    assert_match "isa = PBXNativeTarget", output
+  end
+end
